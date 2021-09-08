0c39e3237723d1f2dc891fee24557a8843d57808
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2021-09-08 18:11:38 +0000

    Set up the display, user and new user funtion

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from datetime import date
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -17,3 +18,36 @@ users = SHEET.worksheet("users")
 users_data = users.get_all_values()
 
 print(users_data)
+
+def display:
+    """
+    Welcome message to the user:
+        -ask information from the user if it is a new or an excisting one
+    """
+
+    today = date.today()
+    print(today)    
+    print("Welcome to the Daily Planner")
+    choice = input("If you are a new user please enter 0 if you already have an account enter 1: /n")
+    return choice
+
+def user:
+    """
+    Asking for user's data
+        -Take user's name and password
+        -Run user's information from the validator of the current entries
+    """
+
+    
+
+
+def new_user:
+    """
+    Asking for new user's data
+        -Ask for username and password
+        -Run user's information from the validator if the username excists or not provide 
+            the correct message and run the process again to get a right username
+    """
+
+def main:
+    us_choice = display()
